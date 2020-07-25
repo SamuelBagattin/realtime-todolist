f0633b9ef648261dc8f98b5cce81ebd09bdad6c9
samuel <samuel.bagattin@gmail.com>
2020-07-25 11:34:07 +0200

    Update dependencies

--- database/connection.js
+++ database/connection.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 let db = mongoose.connection;
 module.exports = db;
